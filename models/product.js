c4795b61814b95a8ca5baaec6511b7889b3fdd5c
HackWorld <iptvhackworld@gmail.com>
2026-05-09 11:31:10 +0200

    add models folder

--- models/product.js
+++ models/product.js
@@ -18,4 +18,7 @@ const productSchema = new mongoose.Schema({
 
 });
 
-module.exports = mongoose.model("Product", productSchema);
+module.exports = mongoose.model(
+    "Product",
+    productSchema
+);
